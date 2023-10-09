b8c5003874097e26863d3d8ef3e05826d8c6a291
Tom Bell <tomb@tomb.io>
2023-10-09 23:31:10 +0100

    Add keymap for dz60rgb_ansi

---
+++ keyboards/dztech/dz60rgb_ansi/keymaps/tombell/rules.mk
@@ -0,0 +1,13 @@
+BOOTLOADER_SIZE = 6144
+
+AUDIO_ENABLE      = no  # Audio output
+BACKLIGHT_ENABLE  = no  # Enable keyboard backlight functionality
+BOOTMAGIC_ENABLE  = yes # Enable Bootmagic Lite
+COMMAND_ENABLE    = no  # Commands for debug and configuration
+CONSOLE_ENABLE    = no  # Console for debug
+EXTRAKEY_ENABLE   = yes # Audio control and System control
+LTO_ENABLE        = yes
+MOUSEKEY_ENABLE   = no  # Mouse keys
+NKRO_ENABLE       = no  # Enable N-Key Rollover
+RGBLIGHT_ENABLE   = no  # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes # Use RGB matrix
